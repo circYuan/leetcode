ef561f9d1b48a2b7b2d61ecaf995e1f5db0a499b
my_vim_setting <45076680+yuan-circle@users.noreply.github.com>
2019-04-25 17:10:03 +0800

    Create question69.cc

---
+++ question69.cc
@@ -0,0 +1,11 @@
+/**************************
+In fact this problem can be solved by using sqrt function.It's a lazy method haha.
+**************************/
+
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        return (int)sqrt(x);
+    }
+};
